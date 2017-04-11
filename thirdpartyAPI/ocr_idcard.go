40f95fc2f655f5b36d9ec3bfaf5175804d905341
ipiao <yukk046@zhixubao.com>
2017-04-11 19:43:48 +0800

    idcard

---
+++ thirdpartyAPI/ocr_idcard.go
@@ -0,0 +1,110 @@
+package metools
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// OcrIDCardInput 印刷字识别-身份证-请求
+type OcrIDCardInput struct {
+	Image struct {
+		DataType  int    `json:"dataType,omitempty"`
+		DataValue string `json:"dataValue,omitempty"` //图片以base64编码的string
+	} `json:"image,omitempty"`
+	Configure struct {
+		DataType  int `json:"dataType,omitempty"`
+		DataValue struct {
+			Side string `json:"side,omitempty"`
+		} `json:"dataValue,omitempty"` //身份证正反面类型:face/back
+	} `json:"configure,omitempty"`
+}
+
+// OcrIDCardInputs 印刷字识别-身份证-请求
+type OcrIDCardInputs struct {
+	Inputs []OcrIDCardInput `json:"inputs,omitempty"`
+}
+
+// OcrIDCardResp 印刷字识别-身份证-返回
+type OcrIDCardResp struct {
+	OutPuts []struct {
+		OutputLabel string
+		OutputMulti struct{}
+		OutputValue struct {
+			DataType  int
+			DataValue string
+		}
+	}
+}
+
+// AliOcrIDCardJSON 阿里印刷字识别-身份证
+func AliOcrIDCardJSON(inputs *OcrIDCardInputs) (string, error) {
+
+	bs, err := json.Marshal(inputs)
+	if err != nil {
+		return "", errors.New("json解析错误")
+	}
+	var baseURL = "http://dm-51.data.aliyun.com/rest/160601/ocr/ocr_idcard.json"
+	cilent := &http.Client{}
+	body := bytes.NewReader(bs)
+	req, _ := http.NewRequest("POST", baseURL, body)
+	req.Header.Add("Authorization", "APPCODE "+aliAppCode)
+	req.Header.Add("Content-Type", "application/json; charset=UTF-8")
+
+	resp, err := cilent.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	rbs, err := ioutil.ReadAll(resp.Body)
+	return string(rbs), err
+}
+
+// AliOcrIDCardStruct 主工具函数
+func AliOcrIDCardStruct(inputs *OcrIDCardInputs) (*OcrIDCardResp, error) {
+	var res = new(OcrIDCardResp)
+	str, err := AliOcrIDCardJSON(inputs)
+	if err != nil {
+		log.Println("http调用错误：", err)
+		return nil, err
+	}
+	err = json.Unmarshal([]byte(str), res)
+	if err != nil {
+		log.Println("json解析错误", err)
+		return nil, err
+	}
+	return res, nil
+}
+
+// TransPicToOcrIDCardInput 将图片转换为结构体
+func TransPicToOcrIDCardInput(pic, picType string) (OcrIDCardInput, error) {
+	var input = OcrIDCardInput{}
+	bs, err := ioutil.ReadFile(pic)
+	if err != nil {
+		return input, err
+	}
+	input.Image.DataType = 50
+	input.Image.DataValue = base64.StdEncoding.EncodeToString(bs)
+	input.Configure.DataType = 50
+	input.Configure.DataValue.Side = picType
+	return input, err
+}
+
+// TransIDCardsPicsToOcrIDCardInput 转换身份证
+func TransIDCardsPicsToOcrIDCardInput(face, back string) (*OcrIDCardInputs, error) {
+	f, err := TransPicToOcrIDCardInput(face, "face")
+	if err != nil {
+		return nil, err
+	}
+	s, err := TransPicToOcrIDCardInput(back, "back")
+	if err != nil {
+		return nil, err
+	}
+	var inputs = new(OcrIDCardInputs)
+	inputs.Inputs = append(inputs.Inputs, f, s)
+	return inputs, nil
+}
---
+++ utils/safe.go
@@ -0,0 +1,50 @@
+package metools
+
+import (
+	"crypto/md5"
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+// NewSalt 生成随机盐
+func NewSalt() string {
+	var seed = rand.New(rand.NewSource(time.Now().UnixNano()))
+	return strconv.Itoa(seed.Int())
+}
+
+// MD5 md5加密
+func MD5(source string) string {
+	h := md5.New()
+	h.Write([]byte(source))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+// CreatePwd 创建密码
+func CreatePwd(sourcePwd string) string {
+	return MD5(sourcePwd)
+}
+
+// CheckPwd 检验密码
+func CheckPwd(pwd, dest string) bool {
+	return MD5(pwd) == dest
+}
+
+// Base64Decode base64 RFC 4648 解密,标准解密
+func Base64Decode(origonStr string) string {
+	var dest []byte
+	dest, err := base64.StdEncoding.DecodeString(origonStr)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	return string(dest)
+}
+
+// Base64Encode base64 RFC 4648 加密,标准加密
+func Base64Encode(origonStr string) string {
+	return base64.StdEncoding.EncodeToString([]byte(origonStr))
+}
