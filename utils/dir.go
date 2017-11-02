f9942d8f6d09e5bfe3a42b69510cc91029ec2527
ipiao <yukk046@zhixubao.com>
2017-11-02 18:39:12 +0800

    change

--- utils/dir.go
+++ utils/dir.go
@@ -9,12 +9,13 @@ import (
 // CheckOrCreateDir 检查或生成路径
 func CheckOrCreateDir(dir string) (string, error) {
 	_, err := ioutil.ReadDir(dir)
-	if err != nil {
+	if os.IsNotExist(err) {
 		err = os.MkdirAll(dir, 0777)
 	}
 	return dir, err
 }
 
+// CreateFile 创建文件
 func CreateFile(fname string) (*os.File, error) {
 	dir := filepath.Dir(fname)
 	_, err := CheckOrCreateDir(dir)
