6d3b4c092146650e2a1abab219bc3810a1f03ada
ipiao <yukk046@zhixubao.com>
2018-03-20 18:43:02 +0800

    bforest2BTree

--- algorithm/tree/binary_tree.go
+++ algorithm/tree/binary_tree.go
@@ -2,24 +2,45 @@ package tree
 
 import "errors"
 
-// 二叉树的一个性质
-
-// BinaryTree 二叉树
-type BinaryTree struct {
+// BinaryNode 二叉树
+type BinaryNode struct {
 	*Node
 }
 
-// NewBinaryTree 新建二叉数
-func NewBinaryTree(val interface{}) *BinaryTree {
-	return &BinaryTree{
+// NewBinaryNode 新建二叉树
+func NewBinaryNode(val interface{}) *BinaryNode {
+	return &BinaryNode{
 		Node: NewNode(val),
 	}
 }
 
 // AddChild 重载函数
-func (bt *BinaryTree) AddChild(child TNode) error {
-	if len(bt.children) == 2 {
+func (bt *BinaryNode) AddChild(child TNode) error {
+	c := child.(*BinaryNode)
+	if bt.Degree() == 2 {
 		return errors.New("already has 2 children")
 	}
-	return bt.Node.AddChild(child)
+	return bt.AddChild(c)
+}
+
+// BinaryThrNode 线索二叉树
+type BinaryThrNode struct {
+	*Node
+	lchild TNode
+	rchild TNode
+}
+
+// AddChild 重载
+func (bt *BinaryThrNode) AddChild(child TNode) error {
+	c := child.(*BinaryThrNode)
+	dg := bt.Degree()
+	if dg == 0 {
+		bt.lchild = c
+	} else if dg == 1 {
+		bt.rchild = c
+	} else {
+		return errors.New("already has 2 children")
+	}
+
+	return bt.AddChild(c)
 }
