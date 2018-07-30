7c5e11cb4b5f28ee33130ffd8810252c109beb89
ykk <yukaokao301@mys4s.cn>
2018-07-30 17:00:25 +0800

    datetool

--- datetool/timejson.go
+++ datetool/timejson.go
@@ -1,21 +1,19 @@
 package datetool
 
 import (
-	"log"
 	"time"
 )
 
 type Time time.Time
 
 const (
-	timeFormart = "2006-01-02T15:04:05Z07:00"
+	timeFormart = "2006-01-02 15:04:05"
 	timeFormat  = "2006-01-02 15:04:05"
 )
 
 func (t *Time) UnmarshalJSON(data []byte) (err error) {
 
 	now, err := time.ParseInLocation(`"`+timeFormart+`"`, string(data), time.Local)
-	log.Println("now:", now)
 	*t = Time(now)
 	return
 }
