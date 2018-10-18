7cb21c8abbf4306152a281a9e1982c9fe6c2e7f5
ipiao <530151330@qq.com>
2018-10-18 14:54:55 +0800

    json

---
+++ mutils/json.go
@@ -0,0 +1,22 @@
+package mutils
+
+import (
+	"bytes"
+	"encoding/json"
+)
+
+// DeJSON decode json to interface
+func DeJSON(data []byte, v interface{}) error {
+	var decode = json.NewDecoder(bytes.NewBuffer(data))
+	decode.UseNumber()
+	return decode.Decode(&v)
+}
+
+// EnJSON 解析成json
+func EnJSON(v interface{}) ([]byte, error) {
+	var bs []byte
+	bf := bytes.NewBuffer(bs)
+	var encode = json.NewEncoder(bf)
+	err := encode.Encode(v)
+	return bf.Bytes(), err
+}
